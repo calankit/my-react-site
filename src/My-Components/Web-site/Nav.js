ea8d4dc539efa8dc169cc4496e20e7270a045cc8
CodeArtisanLab <codeartisanlab2607@gmail.com>
2025-08-05 17:37:10 +0530

    Prepare for GitHub Pages deployment

--- src/My-Components/Web-site/Nav.js
+++ src/My-Components/Web-site/Nav.js
@@ -15,14 +15,18 @@ const Navbar = () => {
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+                    {/* Center Navigation Links */}
                     <ul className="navbar-nav mx-auto gap-4">
                         <li className="nav-item">
-                            <a className="nav-link active" href="">Home</a>
+                            <a className="nav-link active" href="#Home">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#about">About</a>
@@ -38,19 +42,18 @@ const Navbar = () => {
                     {/* User Icon Dropdown */}
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
+                            <button
+                                className="nav-link dropdown-toggle btn btn-link"
                                 id="loginDropdown"
-                                role="button"
+                                type="button"
                                 data-bs-toggle="dropdown"
                                 aria-expanded="false"
                             >
                                 <i className="bi bi-person-circle fs-4"></i>
-                            </a>
+                            </button>
                             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="loginDropdown">
                                 <li>
-                                    <Link className="dropdown-item" to="login">
+                                    <Link className="dropdown-item" to="/login">
                                         Login
                                     </Link>
                                 </li>
